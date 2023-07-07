8c48f4b10a493bf43cd764981fca426cd3113389
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2023-07-08 00:19:08 +0300

    database connection established

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv").config();
 const app = express();
 const router = require('./routes/contactRoutes') ;
 const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnection");
 
+connectDB();
 const port = process.env.PORT || 5000
 app.use(express.json());
 app.use("/api/contacts",router)
